7ab190b259f852267df507186d7a105ada16146e
MalikGallow <gallowabdul@gmail.com>
2019-03-06 16:50:48 +0200

    added project

---
+++ js/main.js
@@ -0,0 +1,103 @@
+// javascript for history
+function getHistory(){
+    return document.getElementById("history-value").innerText;
+}
+
+function printHistory(num){
+    document.getElementById("history-value").innerText=num;
+}
+
+// output
+function getOutput(){
+    return document.getElementById("output-value").innerText;
+}
+
+function printOutput(num){
+    if(num ===""){
+        document.getElementById("output-value").innerText=num;
+    }
+    else{
+        document.getElementById("output-value").innerText=getFormattedNumber(num);
+    }
+    
+}
+
+// function to produce commas in number
+function getFormattedNumber(num){
+    if(num=="-"){
+        return ""
+    }
+    var n = Number (num);
+    var value = n.toLocaleString("en");
+    return value;
+}
+
+function reverseNumberFormat(num){
+    return Number(num.replace(/,/g,''));
+}
+    
+// operators
+var operator = document.getElementsByClassName("operator");
+for(var i =0;i<operator.length;i++){
+	operator[i].addEventListener('click',function(){
+		if(this.id=="clear"){
+			printHistory("");
+			printOutput("");
+		}
+		else if(this.id=="backspace"){
+			var output=reverseNumberFormat(getOutput()).toString();
+			if(output){//if output has a value
+				output= output.substr(0,output.length-1);
+				printOutput(output);
+			}
+		}
+		else{
+			var output=getOutput();
+			var history=getHistory();
+			if(output==""&&history!=""){
+				if(isNaN(history[history.length-1])){
+					history= history.substr(0,history.length-1);
+				}
+			}
+			if(output!="" || history!=""){
+				output= output==""?output:reverseNumberFormat(output);
+				history=history+output;
+				if(this.id=="="){
+					var result=eval(history);
+					printOutput(result);
+					printHistory("");
+				}
+				else{
+					history=history+this.id;
+					printHistory(history);
+					printOutput("");
+				}
+			}
+		}
+		
+	});
+}
+
+
+
+var number = document.getElementsByClassName("number");
+for(var i =0;i<number.length;i++){
+	number[i].addEventListener('click',function(){
+		var output=reverseNumberFormat(getOutput());
+		if(output!=NaN){ //if output is a number
+			output=output+this.id;
+			printOutput(output);
+		}
+	});
+}
+
+function viewCalculator(){
+    var calculate = document.getElementById("calculator")
+    if(calculate.style.display == 'none'){
+        calculate.style.display = 'block';
+    }
+    else{
+        calculate.style.display = 'none';
+    }
+} 
+
